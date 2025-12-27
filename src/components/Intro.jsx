aa7f8d9b2a094cc0f851e688e57d043b91f52f80
AryanAg08 <aryangoyal90@outlook.com>
2025-12-27 18:08:21 +0530

    Fixing animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,44 +1,55 @@
 import React, { Component } from 'react';
 import "../styles/Intro.css";
-import Typist from 'react-typist';
+// import Typist from 'react-typist';
 import EmailRounded from '@mui/icons-material/EmailRounded';
 import DescriptionRoundedIcon from '@mui/icons-material/DescriptionRounded';
 import FadeInSection from './FadeInSection';
+import Typewriter from 'typewriter-effect';
+
 
 
 export class Intro extends Component {
-constructor () {
+  constructor() {
     super();
     this.state = {
-        expanded: true,
-        activeKey: "1",
-        visible: true,
+      expanded: true,
+      activeKey: "1",
+      visible: true,
     };
     this.handleSelect = this.handleSelect.bind(this);
-}
-handleSelect(eventKey) {
+  }
+  handleSelect(eventKey) {
     this.setState({
-        activeKey: eventKey,
+      activeKey: eventKey,
     });
-}
+  }
 
   render() {
     return (
       <div id="intro">
 
-        <Typist avgTypingDelay={120}>
-            <span className='intro-title'>
-                {"hi, "}
-                <span className='intro-name'>{"Aryan "}</span>
-                {"this side."}
-            </span>
-        </Typist>
-        <br/>
-         
-      <FadeInSection>
-      <div className="intro-subtitle">I create stuff sometimes.</div>
+       <Typewriter
+  options={{
+    delay: 120,
+    autoStart: true,
+  }}
+  onInit={(typewriter) => {
+    typewriter
+      .typeString(
+        `<span class='intro-title'>
+          hi, <span class='intro-name'>Aryan </span>this side.
+        </span>`
+      )
+      .start();
+  }}
+/>
+
+        <br />
+
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
           <div className="intro-desc">
-            I'm a Full-Stack Developer. Fascinated with large-scale products and 
+            I'm a Full-Stack Developer. Fascinated with large-scale products and
             new emerging technologies. Ping me if you wanna talk more about technologies!!
           </div>
           <a
@@ -49,13 +60,13 @@ handleSelect(eventKey) {
             {" Say hi!"}
           </a>
           <a
-          href="https://drive.google.com/file/d/10sK-YpsGcYtCG4U2O8QloCz06ExEvtqe/view?usp=sharing"
-          className='intro-resume'
+            href="https://drive.google.com/file/d/10sK-YpsGcYtCG4U2O8QloCz06ExEvtqe/view?usp=sharing"
+            className='intro-resume'
           >
-         <DescriptionRoundedIcon>   </DescriptionRoundedIcon>
-             {" Resume! "}
+            <DescriptionRoundedIcon>   </DescriptionRoundedIcon>
+            {" Resume! "}
           </a>
-          </FadeInSection>
+        </FadeInSection>
       </div>
     )
   }
